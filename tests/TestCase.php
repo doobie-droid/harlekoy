57069409ec852679d5181f8ca7cf21b671bfdca4
doobie-droid <lesliedouglas23@gmail.com>
2024-10-16 14:51:19 +0100

    ci: setup test database for use during testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,31 @@
 
 namespace Tests;
 
+use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function setUpTraits()
+    {
+        $uses = parent::setUpTraits();
+
+        if (isset($uses[Traits\RefreshTestingDatabase::class])) {
+            $this->refreshTestDatabase();
+        }
+
+        return $uses;
+    }
+
+    protected function makeUser(int $times = 1): UserFactory
+    {
+        return UserFactory::times($times);
+    }
 }
