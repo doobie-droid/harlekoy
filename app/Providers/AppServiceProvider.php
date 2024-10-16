a149cc429cd25f82ffb78f0796bff1b321f40490
doobie-droid <lesliedouglas23@gmail.com>
2024-10-17 00:10:06 +0100

    feat: register UserObservers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->bootObservers();
+    }
+
+    private function bootObservers()
+    {
+        $observers = [
+            User::class => UserObserver::class,
+        ];
+
+        foreach ($observers as $model => $observer) {
+            $model::observe($observer);
+        }
     }
 }
